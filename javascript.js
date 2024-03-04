2560ad7b95857764b13fcc99f1c00d028c0a55de
dawrentz <156712569+dawrentz@users.noreply.github.com>
2024-03-04 15:33:29 -0600

    Create js code for comp to choose random
    Done via random number 1-3 and assigning rock, paper or scissors to the value

--- javascript.js
+++ javascript.js
@@ -4,3 +4,20 @@
 //run through all possibilities with if then
 //declare winner
 //let user choose to play again or quit
+
+function getCompterChoice() {
+    let randNum = (Math.floor(Math.random() * 3 + 1));
+
+    let choice;
+    if (randNum === 1) {choice = "rock"}
+    else if (randNum === 2) {choice = "paper"}
+    else {choice = "scissors"}
+
+    
+    return choice;
+//    return randNum;
+}
+
+console.log(getCompterChoice());
+
+
