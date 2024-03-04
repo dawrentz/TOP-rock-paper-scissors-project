86f2ff37f7f6c1bb9ab36ad55e864802c99404b2
dawrentz <156712569+dawrentz@users.noreply.github.com>
2024-03-04 14:38:16 -0600

    Create initial files, html skeleton, and pseudo-code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TOP-rock-paper-scissors-project
+TOP project to practice basic javascript. Running all inside of console
+
+https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
---
+++ javascript.js
@@ -0,0 +1,6 @@
+//allow computer to choose random 1-3 at start
+//assign random choice to rock, paper, scissor
+//prompt users to enter rock, paper, or scissors
+//run through all possibilities with if then
+//declare winner
+//let user choose to play again or quit
