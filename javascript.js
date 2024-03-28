927397f73ddbb6c3f94975f8ac62922d254b6b34
dawrentz <156712569+dawrentz@users.noreply.github.com>
2024-03-28 15:25:11 -0500

    Polish, clean up code, and edit comments

--- javascript.js
+++ javascript.js
@@ -1,15 +1,4 @@
-//allow computer to choose random 1-3 at start
-//assign random choice to rock, paper, scissor
-//prompt users to enter rock, paper, or scissors
-//standardize user prompt
-//run through all possibilities with if then
-//declare winner
-//log result and pass to function for counter
-//run five rounds and display champion
-
-
-//select random number between 1 and 3
-//assign number as rock, paper or scissors
+//select random number between 1 and 3. Assign number as rock, paper or scissors
 function getCompterChoice() {
     let randNum = (Math.floor(Math.random() * 3 + 1));
 
@@ -21,32 +10,18 @@ function getCompterChoice() {
     return choice;
 }
 
-//allow player to select, standardize selection
-function getPlayerChoice() {
-    let playerChoice = prompt("Choose rock, paper, or scissors: ");
-    return (playerChoice.trim()).toUpperCase();
-}
-
-// Create buttons and add eventListen, make click run a game
-
-
+// Make click button = choice and run one game
 const buttons = document.querySelectorAll("button");
+
 buttons.forEach(button => {
     button.addEventListener("click", function() {
         let playerSelection = button.id.toUpperCase();
         let computerSelection = getCompterChoice();
         outcome(playerSelection, computerSelection);
-
-
-        // console.log("playerSelection: " + playerSelection);
-        // console.log("computerSelection: " + computerSelection);
-
     });
-
 });
 
-
-
+// Initialize and display score counters
 let playerScore = 0;
 let computerScore = 0;
 
@@ -56,75 +31,42 @@ const computerScoreDisplay = document.querySelector("#computerScore");
 playerScoreDisplay.textContent = playerScore;
 computerScoreDisplay.textContent = computerScore;
 
-const outcomeBox = document.querySelector("#outcome");
-// outcomeBox.textContent = "Hello!";
 
-//group all like outcomes (including user error)
-//alert verbose outcome and return/log simple outcome
+// Group all like outcomes, and determine outcome and display match result. Add to score counter.
+const outcomeBox = document.querySelector("#outcome");
+
 function outcome(player, computer) {
      //win
     if ((player === "ROCK" && computer === "SCISSORS") ||
         (player === "PAPER" && computer === "ROCK") ||
         (player === "SCISSORS" && computer === "PAPER")    
     ) {
-        // console.log("win"); 
-        // alert(`${player} beats ${computer}!\n\nYou have choosen... WISELY`);          
-        // return "win";    
-        
         outcomeBox.textContent = `${player} beats ${computer}!\n\nYou have choosen... WISELY`;
         playerScore = ++playerScore;
         playerScoreDisplay.textContent = playerScore;
-
-
     } 
     //lose
     else if ((computer === "ROCK" && player === "SCISSORS") ||
     (computer === "PAPER" && player === "ROCK") ||
     (computer === "SCISSORS" && player === "PAPER")    
     ) {
-        // console.log("loss");
-        // alert(`${computer} beats ${player}!\n\nYou have choosen... POORLY`);
-        // return "loss";
-
         outcomeBox.textContent = `${computer} beats ${player}!\n\nYou have choosen... POORLY`;
         computerScore = ++computerScore;
         computerScoreDisplay.textContent = computerScore;
-
     } 
     //tie
     else if (player === computer) {
-        // console.log("tie");
-        // alert(`${computer} = ${player}\n\nMEDIOCRE`);
-        // return "tie";
-
         outcomeBox.textContent = `${computer} = ${player}... \n\nMEDIOCRE`;
-
     }
-    //error
-    else {
-        console.log("error");
-        alert('*sigh*\n\nYou may only select "rock", "paper", or "scissors"...\n\nRefresh and try again');
-        return "error";
-    }  
-
 }
 
-//combine functions for a single round
-//return simple round outcome (to pass to counter)
-function playRound() {
-    let computerSelection = getCompterChoice();
-    // console.log(computerSelection); //cheatmode
-    // let playerSelection = getPlayerChoice();
-    return outcome(playerSelection, computerSelection);
-}
-
-
-const body = document.querySelector("body");
+// Check counters on each click for 5 wins and declare champion
 const buttonsGroup = document.querySelector(".buttons");
+const note = document.querySelector("span");
 
-document.querySelector(".buttons").addEventListener("click", function() {
+buttonsGroup.addEventListener("click", function() {
     if (playerScore == 5 || computerScore == 5) {
-        body.removeChild(buttonsGroup);
+        document.querySelector("body").removeChild(buttonsGroup);
         if (playerScore > computerScore) {
             playerScoreDisplay.textContent = "WINNER!!!";
             playerScoreDisplay.style.color = "red";
@@ -132,7 +74,7 @@ document.querySelector(".buttons").addEventListener("click", function() {
             computerScoreDisplay.textContent = "WINNER!!!";
             computerScoreDisplay.style.color = "red";
         }
-        document.querySelector("span").textContent = "Hit refresh to play again";
-        document.querySelector("span").style.cssText = "color: green; font-size: x-large;";
+        note.textContent = "Hit refresh to play again";
+        note.style.cssText = "color: green; font-size: x-large;";
     }
 });
